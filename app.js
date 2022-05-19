557cbb7ff11c10b3af96d72b424cbf81412971a8
Damjan <damjandimitrov1@gmail.com>
2022-05-19 21:23:19 +0200

    Updates

--- app.js
+++ app.js
@@ -1,44 +1,21 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(3000, () => console.log(`Hackathon app listening on port 3000`));
+
+const config = require('dotenv').config().parsed;
 const { MongoClient } = require('mongodb');
 
 app.post('/addNew', async (req, res) => {
-    const name = "Damjan";
+    const name = req.body.name;
+    const client = new MongoClient(config.MONGO_DB_URL);
     await client.connect();
     console.log('Connected successfully to server');
     const db = client.db('Cluster0');
     const collection = db.collection('newCollection');
     await collection.insertOne({ name });
 
-    res.send('Hello World!')
+    res.send('Success!')
 });
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
-// or as an es module:
-// import { MongoClient } from 'mongodb'
-
-// Connection URL
-console.log(process.env.MONGO_DB);
-const url = 'mongodb+srv://hackathon:hackathon_admin@cluster0.wjyl3.mongodb.net/?retryWrites=true&w=majority';
-const client = new MongoClient(url);
-
-// Database Name
-const dbName = 'Cluster0';
-
-async function main() {
-    // Use connect method to connect to the server
-
-
-    // the following code examples can be pasted here...
-
-    return 'done.';
-}
-
-// main()
-//     .then(console.log)
-//     .catch(console.error)
-//     .finally(() => client.close());
