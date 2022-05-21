204ee49151cb4d93dbac2cde09f1e5e86fceb336
DP <89171103@student.upr.si>
2022-05-21 21:23:30 +0200

    /register /login /diets /allergens /meals

--- backend/routes.js
+++ backend/routes.js
@@ -3,19 +3,6 @@ const { MongoClient } = require('mongodb');
 const express = require('express');
 const router = express.Router();
 
-
-router.post('/addNew', async (req, res) => {
-    const name = req.body.name;
-    const client = new MongoClient(config.MONGO_DB_URL);
-    await client.connect();
-    console.log('Connected successfully to server');
-    const db = client.db('Cluster0');
-    const collection = db.collection('newCollection');
-    await collection.insertOne({ name });
-
-    res.send('Success!')
-});
-
 router.post('/register', async (req, res) => {
     const name = req.body.name;
     const password = req.body.password;
@@ -23,12 +10,70 @@ router.post('/register', async (req, res) => {
     const categories = req.body.categories;
     const client = new MongoClient(config.MONGO_DB_URL);
     await client.connect();
-    console.log('Connected successfully to server');
     const db = client.db('Cluster0');
     const collection = db.collection('users');
     await collection.insertOne({ name , password, email, categories });
+    console.log(req.body);
 
-    res.send('Success!')
+    res.send('Register Success!')
 });
+
+router.post('/login', async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const client = new MongoClient(config.MONGO_DB_URL);
+    await client.connect();
+    const db = client.db('Cluster0');
+    const collection = db.collection('users');
+    const user = await collection.findOne({ password, email });
+    if(user == null) {
+        res.status(400).send("User not exist");
+    }
+    res.status(200).send('Login Success!');
+});
+
+
+router.get('/diets', async (req, res) => {
+    const client = new MongoClient(config.MONGO_DB_URL);
+    await client.connect();
+    const db = client.db('Cluster0');
+    
+    db.collection("foodDiets").find({}).toArray(function(err, result) {
+        if (err) return res.status(400).send("Empty");
+        return res.status(200).send(result);
+    
+      });
+    });
+
+    router.get('/allergens', async (req, res) => {
+        const client = new MongoClient(config.MONGO_DB_URL);
+        await client.connect();
+        const db = client.db('Cluster0');
+        
+        db.collection("allergens").find({}).toArray(function(err, result) {
+            if (err) return res.status(400).send("Empty");
+            return res.status(200).send(result);
+        
+          });
+        });
+
+
+        router.get('/meals', async (req, res) => {
+            const client = new MongoClient(config.MONGO_DB_URL);
+            await client.connect();
+            const db = client.db('Cluster0');
+            
+            db.collection("meals").find({}).toArray(function(err, result) {
+                if (err) return res.status(400).send("Empty");
+                return res.status(200).send(result);
+            
+              });
+            });
+    
+    //if(foodDiets == null) res.status(400).send({});
+
+    
+
+
 
 module.exports = { router };
