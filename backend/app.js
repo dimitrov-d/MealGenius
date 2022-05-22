262a4bec02560bc78c0d08097cbca75a0e38a88a
Riste Micev <riste.micev1@gmail.com>
2022-05-22 11:43:50 +0200

    implemented jwt authentication

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 const authRouter = require('./routes/auth');
 const mealsRouter = require('./routes/meals');
