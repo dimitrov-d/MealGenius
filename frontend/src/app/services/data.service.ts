1d14408a3b12c8e7beafd5fbccd64924f6084816
Damjan <damjandimitrov1@gmail.com>
2022-05-22 11:40:30 +0200

    Implement shopping list

--- frontend/src/app/services/data.service.ts
+++ frontend/src/app/services/data.service.ts
@@ -2,6 +2,7 @@ import { Meal } from './../shared/models/Meal';
 import { ErrorHandlerService } from '@services/error-handler.service';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ShoppingList } from '@shared/models/ShoppingList';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,11 @@ export class DataService {
 
   getAllMeals(callback: (meals: Meal[]) => void) {
     this.errorHandler.addErrorHandler(this.http.get('http://localhost:3000/collections/meals'))
+      .subscribe(callback);
+  }
+
+  getShoppingLists(callback: (meals: ShoppingList[]) => void) {
+    this.errorHandler.addErrorHandler(this.http.get('http://localhost:3000/collections/shopping-list'))
       .subscribe(callback);
   }
 }
